c9a3efd44b1401d6370a9e16e8717984f19e9d1c
Hapuarachchige Minaga Methyuga <133320917+MinagaMethyuga@users.noreply.github.com>
2025-12-30 01:00:06 +0530

    fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash; // ← Add this
 use Illuminate\Support\Str;
 
 /**
@@ -26,7 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make('password'), // ← REQUIRED
+            'password' => Hash::make('password'), // ← Works now
             'remember_token' => Str::random(10),
         ];
     }
