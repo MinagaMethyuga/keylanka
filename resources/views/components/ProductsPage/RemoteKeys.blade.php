0e71d65251857068028f37f92de6767766508160
Hapuarachchige Minaga Methyuga <133320917+MinagaMethyuga@users.noreply.github.com>
2026-01-02 02:36:32 +0530

    items stock tab fix

--- resources/views/components/ProductsPage/RemoteKeys.blade.php
+++ resources/views/components/ProductsPage/RemoteKeys.blade.php
@@ -37,7 +37,7 @@
         <p class="text-white text-base font-medium leading-normal pb-2">Stock(Amount)</p>
         <div class="relative">
             <span class="absolute inset-y-0 left-0 flex items-center pl-4 text-[#92a4c9]"></span>
-            <input class="form-input pl-8 w-full rounded-lg text-white focus:outline-0 focus:ring-2 focus:ring-primary border border-[#324467] bg-[#101622] h-14 placeholder:text-[#92a4c9] p-[15px] text-base font-normal leading-normal transition-all" placeholder="00" type="number" value="" name="Stock"/>
+            <input class="form-input pl-8 w-full rounded-lg text-white focus:outline-0 focus:ring-2 focus:ring-primary border border-[#324467] bg-[#101622] h-14 placeholder:text-[#92a4c9] p-[15px] text-base font-normal leading-normal transition-all" placeholder="00" type="number" value="" name="stock"/>
         </div>
     </div>
     <div class="md:col-span-2">
